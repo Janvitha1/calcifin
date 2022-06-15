a6c839b092786e35095ee8a33646225cf8e89c7f
Janvitha Kandula <0005446@Trulyzen.local>
2022-06-15 18:42:24 +0530

    first commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Index(string firstNumber, string secondNumber, string Cal)
+
+        {
+            try
+            {
+              if (!(string.IsNullOrEmpty(firstNumber) || string.IsNullOrEmpty(secondNumber)))
+
+                {
+
+
+                    double a = Double.Parse(firstNumber);
+                    double b = Double.Parse(secondNumber);
+                    double result = 0
+                        ;
+
+                    if (Cal == "Add")
+                    {
+                        result = a + b;
+                    }
+                    else if (Cal == "Sub")
+                    {
+                        result = a - b;
+
+                    }
+                    else if (Cal == "Mul")
+                    {
+                        result = a * b;
+                    }
+
+
+                    else if (Cal == "Div")
+                    {
+
+                        result = a / b;
+                    }
+
+
+
+                    ViewBag.Result = result;
+
+                }
+
+                else
+                {
+                    ViewBag.Error = "Please enter values";
+                }
+            }
+            catch (FormatException )
+            {
+                ViewBag.Error  = "please enter valid number";
+            }
+           
+
+            return View();
+        }
+
+    
+        }
+            
+        } 
+
+
+        
+    
+
---
+++ WebApplication3/Models/Calci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Calci
+    {
+
+     
+        
+        public double a { get; set; }
+
+
+
+        
+        public double b { get; set; }
+
+        public int result { get; set; }
+       
+
+        
+    }
+}
